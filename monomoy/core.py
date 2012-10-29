f786057e223da7a988d5f57d09dcdb1ea6993d71
Paul Tagliamonte <paultag@debian.org>
2012-10-28 22:24:37 -0400

    adding in basics.

--- monomoy/core.py
+++ monomoy/core.py
@@ -18,3 +18,10 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+import os
+from pymongo import Connection
+
+_db_name = os.environ.get('MONOMOY_DB', 'monomoy')
+
+connection = Connection('localhost', 27017)
+db = getattr(connection, _db_name)
